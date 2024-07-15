9ec7ff885202cd554ce91fef502b598dc5dc322e
rohitrodge <webrohit27@gmail.com>
2024-07-15 23:46:43 +0530

    add experience

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
       <Navbar/>
       <Hero/>
       <Portfolio/>
-      {/* <Experience /> */}
+      <Experience />
       <Contact/>
       <About/>
       <Footer/>
