554515c8683c2cb3122294093054854e37e735f3
rohitrodge <webrohit27@gmail.com>
2024-07-15 23:48:49 +0530

    add internship section

--- portfolio/src/components/Experience.jsx
+++ portfolio/src/components/Experience.jsx
@@ -16,10 +16,10 @@ const Experience = () => {
         <div class="h-[50px] w-[2px] bg-slate-400 my-1 mx-auto"></div>
 
         <div class="mb-[20px] text-white bg-gray-700/20 p-4 rounded-3xl max-w-[300px] sm:max-w-[600px] mx-auto">
-            <p>Sevenmentor </p>
-            <p className='text-gray-400'>(2020 - Present)</p>
+            <p> SevenMentor & Training Pvt Ltd</p>
+            <p className='text-gray-400'>(2022 - Present)</p>
             <p className='text-gray-500'>
-                Description of your experience in this company.
+                
             </p>
         </div>
 
